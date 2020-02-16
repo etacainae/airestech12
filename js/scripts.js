4a5634fa24d78ff4112b05614ab9a150ed15416c
site-for-design <podkudahka@gmail.ru>
2020-02-17 01:47:06 +0300

    asa

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$('#devices .img-slider').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   // dots: true,
+   // arrows: true,
+   // lazyLoad: 'ondemand',
+   // responsive: [
+   //    {
+   //      breakpoint: 1024,
+   //      settings: {
+   //        slidesToShow: 3,
+   //        slidesToScroll: 3,
+   //        infinite: true,
+   //        dots: true
+   //      }
+   //    },
+   //    {
+   //      breakpoint: 600,
+   //      settings: {
+   //        slidesToShow: 2,
+   //        slidesToScroll: 2
+   //      }
+   //    }
+   //  ],
+   prevArrow: '.devices-arrow.prev-arrow',
+   nextArrow: '.devices-arrow.next-arrow'
+  });

afe8f7052bef33e91789d203d186f28bcd61682c
site-for-design <podkudahka@gmail.ru>
2020-02-23 23:11:17 +0300

    dd

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,22 @@
-
-/* Звёздный рейтинг товаров */
-$.fn.rater = function (options) {
-    var opts = $.extend({}, $.fn.rater.defaults, options);
-    return this.each(function () {
-        var $this = $(this);
-        var $on = $this.find('.rating_starOn');
-        var $off = $this.find('.rating_starOff');
-        opts.size = $on.height();
-        if (opts.rating == undefined) opts.rating = $on.width() / opts.size;
-
-        $off.mousemove(function (e) {
-            var left = e.clientX - $off.offset().left;
-            var width = $off.width() - ($off.width() - left);
-            width = Math.ceil(width / (opts.size / opts.step)) * opts.size / opts.step;
-            $on.width(width);
-        }).hover(function (e) { $on.addClass('rating_starHover'); }, function (e) {
-            $on.removeClass('rating_starHover'); $on.width(opts.rating * opts.size);
-        }).click(function (e) {
-            var r = Math.round($on.width() / $off.width() * (opts.units * opts.step)) / opts.step;
-            $off.unbind('click').unbind('mousemove').unbind('mouseenter').unbind('mouseleave');
-            $off.css('cursor', 'default'); $on.css('cursor', 'default');
-            opts.id = $this.attr('id');
-            $.fn.rater.rate($this, opts, r);
-        }).css('cursor', 'pointer'); $on.css('cursor', 'pointer');
-    });
-};
-
-$('#devices .img-slider').slick({
+$('#devices .devices-slider__wrap').slick({
    slidesToShow: 1,
    slidesToScroll: 1,
-   // dots: true,
-   // arrows: true,
-   // lazyLoad: 'ondemand',
-   // responsive: [
-   //    {
-   //      breakpoint: 1024,
-   //      settings: {
-   //        slidesToShow: 3,
-   //        slidesToScroll: 3,
-   //        infinite: true,
-   //        dots: true
-   //      }
-   //    },
-   //    {
-   //      breakpoint: 600,
-   //      settings: {
-   //        slidesToShow: 2,
-   //        slidesToScroll: 2
-   //      }
-   //    }
-   //  ],
    prevArrow: '.devices-arrow.prev-arrow',
    nextArrow: '.devices-arrow.next-arrow'
   });
+
+$('.structure-img__numbers .number').hover(function() {
+  $('#' + $(this).data('id')).addClass('hover');
+}, function() {
+  $('#' + $(this).data('id')).removeClass('hover');
+});
+$('.structure-list__item').hover(function() {
+  $('[data-id="'+$(this).attr('id')+'"]').addClass('hover');
+}, function() {
+  $('[data-id="'+$(this).attr('id')+'"]').removeClass('hover');
+});
+
+
 $('.faqs-list__item .question').click(function(){
   $('.faqs-list__item .answer').not($(this).next()).slideUp('slow');
   $('.faqs-list__item .elem-condition').removeClass('active');
@@ -80,4 +44,14 @@ responsive: [{
         arrows: false,
     } 
 }]
+});
+$(document).ready(function() {
+   $(".plus").click(function(){
+        var input = $(this).siblings(".amount-input");
+          input[0].stepUp();
+   });
+   $(".minus").click(function(){
+        var input = $(this).siblings(".amount-input");
+          input[0].stepDown();
+   });
 });
